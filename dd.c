35d53e750722dc4959c6b813e9f16d6084e6b94c
Md Bokul Mia <mdbokulmia686@gmail.com>
2025-01-27 14:38:24 +0600

    text file added by me

---
+++ dd.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main() 
+{
+    // char name[50];
+    // scanf("%s", name);
+    // printf("Hello, %s\n", name);
+
+  char name[100];
+  scanf("%s", name); 
+  printf("Hello, ");  
+  printf("%s\n", name);  
+
+    
+    return 0;
+}
